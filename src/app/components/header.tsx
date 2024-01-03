a221f4ad79c60310ce0e1d7903bcbaf8e5a38325
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-03 18:37:05 -0300

    :sparkles: feat: add header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="flex p-5 items-center justify-center gap-5 bg-paynes-gray border-b border-columbia-blue-white drop-shadow-sm ">
+      <span className="text-2xl">🌴</span>
+      <h1 className="font-medium text-2xl uppercase">Trip Items</h1>
+      <span className="text-2xl">💼</span>
+    </header>
+  );
+};
+
+export default Header;
