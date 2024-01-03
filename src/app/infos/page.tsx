e2e42881dca1be2c3aecc4ffbe7192a218ffec4f
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-03 19:01:47 -0300

    :sparkles: feat: add infos page

---
+++ src/app/infos/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Button from "../components/button";
+
+const Infos = () => {
+  return (
+    <div className="flex flex-col gap-4 p-3">
+      <h2 className="text-justify">
+        Os dados são armazenados no localStorage do seu dispositivo, caso tente
+        acessar por outro dispositivo não terá as mesmas informações.{" "}
+      </h2>
+      <Link href={"/"} className="w-full">
+        <Button className="w-full">Voltar a tela inicial</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Infos;
